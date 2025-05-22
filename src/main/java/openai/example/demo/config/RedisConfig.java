44e57bad958a66aad077825f187a1b24b24ba4f1
hcg0127 <kej431003@gmail.com>
2025-05-22 23:28:27 +0900

    [FEAT] Redis 관련 코드 추가

---
+++ src/main/java/openai/example/demo/config/RedisConfig.java
@@ -0,0 +1,29 @@
+package openai.example.demo.config;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.redis.connection.RedisStandaloneConfiguration;
+import org.springframework.data.redis.connection.lettuce.LettuceConnectionFactory;
+import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.data.redis.core.StringRedisTemplate;
+
+@Configuration
+public class RedisConfig {
+
+    @Value("${spring.data.redis.host}")
+    private String host;
+
+    @Value("${spring.data.redis.port}")
+    private int port;
+
+    @Bean
+    public LettuceConnectionFactory lettuceConnectionFactory() {
+        return new LettuceConnectionFactory(new RedisStandaloneConfiguration(host, port));
+    }
+
+    @Bean
+    public RedisTemplate<String, String> redisTemplate() {
+        return new StringRedisTemplate(lettuceConnectionFactory());
+    }
+}
